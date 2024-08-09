76b28993e34c87b63585ee30e4cf79d67f4db9a8
Lazizbek <oddiybolakay05@gmail.com>
2024-08-10 04:37:35 +0500

    api

--- api/json-server.js
+++ api/json-server.js
@@ -1,20 +1,15 @@
 // api/json-server.js
-import { createServer, Model } from 'json-server';
+import jsonServer from 'json-server';
 import fs from 'fs';
 import path from 'path';
 
-const server = createServer();
-const router = createRouter();
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve('.', 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 });
-
-function createRouter() {
-  const filePath = path.resolve('.', 'db.json');
-  const db = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-  return jsonServer.router(db);
-}
