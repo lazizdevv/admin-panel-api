a00e29774136c7aa635084deefe1d3a00492dde5
Lazizbek <oddiybolakay05@gmail.com>
2024-08-11 23:37:34 +0500

    api

--- api/authenticate.js
+++ api/authenticate.js
@@ -2,10 +2,10 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res) => {
-  const { username, password } = req.body;
+  const { name, password } = req.body;
 
   // Foydalanuvchi ma'lumotlarini tekshirish
-  if (username === "admin@gmail.com" && password === 1234) {
+  if (name === "admin@gmail.com" && password === 1234) {
     const token = jwt.sign({ username }, "your-secret-key", {
       expiresIn: "1h",
     });
